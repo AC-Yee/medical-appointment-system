257e01689f556ddf3668a98727b59543b4364a32
AC-Yee <yuguojinxiang@gmail.com>
2025-11-10 20:07:20 +0800

    Add files via upload
    Models a general practitioner who provides primary healthcare and general medical advice.

---
+++ GeneralPractitioner.java
@@ -0,0 +1,24 @@
+// src/GeneralPractitioner.java
+/**
+ * Subclass of HealthProfessional representing a General Practitioner.
+ */
+public class GeneralPractitioner extends HealthProfessional {
+    private boolean acceptsWalkIns;
+
+    public GeneralPractitioner() {
+        super();
+        this.acceptsWalkIns = false;
+    }
+
+    public GeneralPractitioner(int id, String name, String clinicLocation, boolean acceptsWalkIns) {
+        super(id, name, clinicLocation);
+        this.acceptsWalkIns = acceptsWalkIns;
+    }
+
+    @Override
+    public void printDetails() {
+        System.out.println("Type: General Practitioner");
+        super.printDetails();
+        System.out.println("Accepts Walk-ins: " + (acceptsWalkIns ? "Yes" : "No"));
+    }
+}
